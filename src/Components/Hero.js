b6e50a7a4dfa62b25de7b4e1a10a837034868391
acktreptow <acktreptow@gmail.com>
2023-06-05 08:28:53 +0100

    update hero flexbox

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -4,14 +4,18 @@ import "./Hero.css";
 function Hero() {
   return (
     <div className="hero-container">
-      <h1 className="hero-title">Little Lemon</h1>
-      <h2 className="hero-subtitle">Chicago</h2>
-      <p className="hero-info">
-        We are a family owned Mediterranean restaurant, focused on traditional
-        recipes served with a modern twist.
-      </p>
-      <button className="hero-button">Reserve a table</button>
-      <img src={bread} alt="bread" className="hero-img" />
+      <div className="hero-description">
+        <h1 className="hero-title">Little Lemon</h1>
+        <h2 className="hero-subtitle">Chicago</h2>
+        <p className="hero-info">
+          We are a family owned Mediterranean restaurant, focused on traditional
+          recipes served with a modern twist.
+        </p>
+        <button className="hero-button">Reserve a table</button>
+      </div>
+      <div className="hero-img-container">
+        <img src={bread} alt="bread" className="hero-img" />
+      </div>
     </div>
   );
 }
