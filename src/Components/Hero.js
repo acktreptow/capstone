48f3dc43e2aa2f44babb359de976549a01a94ab7
acktreptow <acktreptow@gmail.com>
2023-06-05 03:02:18 +0100

    update hero section

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -3,14 +3,14 @@ import "./Hero.css";
 
 function Hero() {
   return (
-    <div className="hero">
-      <h1 className="title">Little Lemon</h1>
-      <h2 className="subtitle">Chicago</h2>
-      <p className="info">
+    <div className="hero-container">
+      <h1 className="hero-title">Little Lemon</h1>
+      <h2 className="hero-subtitle">Chicago</h2>
+      <p className="hero-info">
         We are a family owned Mediterranean restaurant, focused on traditional
         recipes served with a modern twist.
       </p>
-      <button className="reserve-button">Reserve a table</button>
+      <button className="hero-button">Reserve a table</button>
       <img src={bread} alt="bread" className="hero-img" />
     </div>
   );
