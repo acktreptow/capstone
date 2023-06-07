d6542f4fba8d1ea1016f9cf24f3ad1063316f29c
acktreptow <acktreptow@gmail.com>
2023-06-07 05:48:54 +0100

    update footer component

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,10 +3,12 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="footer-container">
-        <img src={logoWhite} alt="white logo" className="logo-white" />
-        <div className="navigation">
+    <div className="footer-container">
+      <div className='footer-image-container'>
+        <img src={logoWhite} alt="white logo" className="footer-logo" />
+      </div>
+      <div className="footer-links">
+        <div className="footer-navigation-container">
           <h1>Navigation</h1>
           <a href="placeholder.com">Home</a>
           <a href="placeholder.com">About</a>
@@ -15,13 +17,13 @@ function Footer() {
           <a href="placeholder.com">Order Online</a>
           <a href="placeholder.com">Login</a>
         </div>
-        <div className="contact">
+        <div className="footer-contact-container">
           <h1>Contact</h1>
           <a href="placeholder.com">Address</a>
           <a href="placeholder.com">Phone Number</a>
           <a href="placeholder.com">Email</a>
         </div>
-        <div className="social-media">
+        <div className="footer-socialMedia-container">
           <h1>Social Media</h1>
           <a href="placeholder.com">Facebook</a>
           <a href="placeholder.com">Instagram</a>
@@ -29,7 +31,7 @@ function Footer() {
           <a href="placeholder.com">YouTube</a>
         </div>
       </div>
-    </footer>
+    </div>
   );
 }
 
