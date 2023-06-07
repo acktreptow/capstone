98262f8b94d66acec3479f9aea454d69e41f0fc9
acktreptow <acktreptow@gmail.com>
2023-06-07 03:54:57 +0100

    update about section structure

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,22 +4,26 @@ import "./About.css";
 
 function About() {
   return (
-    <div className="about-section">
-      <h1 className="title">Little Lemon</h1>
-      <h2 className="about-subtitle">Chicago</h2>
-      <p className="about-info">
-        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-        sint. Velit officia consequat duis enim velit mollit. Exercitation
-        veniam consequal sunt nostrud amet. Amet minim mollit non deserunt
-        ullamco est sit aliqua dolor do amet sint. Vellt officia consequat duis
-        enim velit mollit.
-      </p>
-      <img src={adrian} alt="Adrian the co-owner" className="adrian-img" />
-      <img
-        src={marioAdrian}
-        alt="Adrian and Mario of Little Lemon"
-        className="adrian-mario-img"
-      />
+    <div className="about-container">
+      <div className="about-description">
+        <h1 className="about-title">Little Lemon</h1>
+        <h2 className="about-subtitle">Chicago</h2>
+        <p className="about-info">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim velit mollit. Exercitation
+          veniam consequal sunt nostrud amet. Amet minim mollit non deserunt
+          ullamco est sit aliqua dolor do amet sint. Vellt officia consequat
+          duis enim velit mollit.
+        </p>
+      </div>
+      <div className="about-images">
+        <img
+          src={marioAdrian}
+          alt="Adrian and Mario of Little Lemon"
+          className="adrian-mario-img"
+        />
+        <img src={adrian} alt="Adrian the co-owner" className="adrian-img" />
+      </div>
     </div>
   );
 }
