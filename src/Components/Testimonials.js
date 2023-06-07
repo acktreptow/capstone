929eb48cb95b3465d96bf8f8f2c3234a89f8c339
acktreptow <acktreptow@gmail.com>
2023-06-07 02:24:14 +0100

    reformated testimonial cards

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -2,7 +2,7 @@ import user1 from "../assets/User1.png";
 import user2 from "../assets/User2.png";
 import user3 from "../assets/User3.png";
 import user4 from "../assets/User4.png";
-import { faStar, faStarHalfStroke } from "@fortawesome/free-solid-svg-icons";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Testimonials.css";
 
@@ -13,13 +13,6 @@ function Testimonials() {
       <div>
         <div className="testimonials">
           <div className="testimonial-card">
-            <img src={user1} alt="first review" className="user-img" />
-            <h2 className="user-name">Jane Doe</h2>
-            <p className="user-text">
-              Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in
-              massa egestas mollis varius dignissim elementum.Mollis tincidunt
-              mattis hendrerit dolor eros enim, nisi ligula ornare.
-            </p>
             <div className="user-rating">
               <FontAwesomeIcon icon={faStar} />
               <FontAwesomeIcon icon={faStar} />
@@ -27,46 +20,61 @@ function Testimonials() {
               <FontAwesomeIcon icon={faStar} />
               <FontAwesomeIcon icon={faStar} />
             </div>
+            <div className="img-name-container">
+              <img src={user1} alt="first review" className="user-img" />
+              <div className="name-username-container">
+                <h2 className="testimonial-name">Jane Doe</h2>
+                <h2 className="testimonial-user-name">Jane92</h2>
+              </div>
+            </div>
+            <p className="user-text">
+              "Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in
+              massa egestas mollis varius dignissim elementum. Mollis tincidunt
+              mattis hendrerit dolor eros enim, nisi ligula ornare."
+            </p>
           </div>
           <div className="testimonial-card">
-            <img src={user2} alt="first review" className="user-img" />
-            <h2 className="user-name">John Doe</h2>
-            <p className="user-text">
-              Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in
-              massa egestas mollis varius dignissim elementum.Mollis tincidunt
-              mattis hendrerit dolor eros enim, nisi ligula ornare.
-            </p>
             <div className="user-rating">
               <FontAwesomeIcon icon={faStar} />
               <FontAwesomeIcon icon={faStar} />
               <FontAwesomeIcon icon={faStar} />
               <FontAwesomeIcon icon={faStar} />
             </div>
+            <div className="img-name-container">
+              <img src={user2} alt="first review" className="user-img" />
+              <div className="name-username-container">
+                <h2 className="testimonial-name">Jon Doe</h2>
+                <h2 className="testimonial-user-name">Jonny_Utah</h2>
+              </div>
+            </div>
+            <p className="user-text">
+              "Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in
+              massa egestas mollis varius dignissim elementum. Mollis tincidunt
+              mattis hendrerit dolor eros enim, nisi ligula ornare."
+            </p>
           </div>
           <div className="testimonial-card">
-            <img src={user3} alt="first review" className="user-img" />
-            <h2 className="user-name">Sara Lopez</h2>
-            <p className="user-text">
-              Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in
-              massa egestas mollis varius dignissim elementum.Mollis tincidunt
-              mattis hendrerit dolor eros enim, nisi ligula ornare.
-            </p>
             <div className="user-rating">
               <FontAwesomeIcon icon={faStar} />
               <FontAwesomeIcon icon={faStar} />
               <FontAwesomeIcon icon={faStar} />
               <FontAwesomeIcon icon={faStar} />
-              <FontAwesomeIcon icon={faStarHalfStroke} />
+              <FontAwesomeIcon icon={faStar} />
             </div>
+            <div className="img-name-container">
+              <img src={user3} alt="first review" className="user-img" />
+              <div className="name-username-container">
+                <h2 className="testimonial-name">Sara Lopez</h2>
+                <h2 className="testimonial-user-name">Sara72</h2>
+              </div>
+            </div>
+            <p className="user-text">
+              "Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in
+              massa egestas mollis varius dignissim elementum. Mollis tincidunt
+              mattis hendrerit dolor eros enim, nisi ligula ornare."
+            </p>
           </div>
           <div className="testimonial-card">
-            <img src={user4} alt="first review" className="user-img" />
-            <h2 className="user-name">Adam Duong</h2>
-            <p className="user-text">
-              Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in
-              massa egestas mollis varius dignissim elementum.Mollis tincidunt
-              mattis hendrerit dolor eros enim, nisi ligula ornare.
-            </p>
             <div className="user-rating">
               <FontAwesomeIcon icon={faStar} />
               <FontAwesomeIcon icon={faStar} />
@@ -74,6 +82,18 @@ function Testimonials() {
               <FontAwesomeIcon icon={faStar} />
               <FontAwesomeIcon icon={faStar} />
             </div>
+            <div className="img-name-container">
+              <img src={user4} alt="first review" className="user-img" />
+              <div className="name-username-container">
+                <h2 className="testimonial-name">Duc Duong</h2>
+                <h2 className="testimonial-user-name">ducky_D</h2>
+              </div>
+            </div>
+            <p className="user-text">
+              "Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in
+              massa egestas mollis varius dignissim elementum. Mollis tincidunt
+              mattis hendrerit dolor eros enim, nisi ligula ornare."
+            </p>
           </div>
         </div>
       </div>
