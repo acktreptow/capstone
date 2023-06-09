28c49b509429277d152ac118605d1ca266171148
acktreptow <acktreptow@gmail.com>
2023-06-09 04:59:21 +0100

    create form

--- src/pages/Booking/BookingForm.js
+++ src/pages/Booking/BookingForm.js
@@ -1,7 +1,36 @@
+import "./BookingForm.css";
+
 function BookingForm() {
   return (
     <div>
-      <h1>Booking Form!</h1>
+      <form className="booking-form">
+        <label htmlFor="res-date">Choose date</label>
+        <br />
+        <input type="date" id="res-date" />
+        <br />
+        <label htmlFor="res-time">Choose time</label>
+        <br />
+        <select id="res-time">
+          <option>17:00</option>
+          <option>18:00</option>
+          <option>19:00</option>
+          <option>20:00</option>
+          <option>21:00</option>
+          <option>22:00</option>
+        </select>{" "}
+        <br />
+        <label htmlFor="guests">Number of guests</label>
+        <br />
+        <input type="number" placeholder="1" min="1" max="10" id="guests" />
+        <br />
+        <label htmlFor="occasion">Occasion</label>
+        <br />
+        <select id="occasion">
+          <option>Birthday</option>
+          <option>Anniversary</option>
+        </select>
+        <input type="submit" value="Make Your reservation" />
+      </form>
     </div>
   );
 }
