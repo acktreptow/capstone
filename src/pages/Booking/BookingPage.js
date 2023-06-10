623e02b27b66f332fdddfba0ae39f823e8b843fa
acktreptow <acktreptow@gmail.com>
2023-06-10 05:45:19 +0100

    create upDateTimes function

--- src/pages/Booking/BookingPage.js
+++ src/pages/Booking/BookingPage.js
@@ -1,6 +1,6 @@
 import "./BookingPage.css";
 import BookingForm from "./BookingForm";
-import { useState } from "react";
+import { useState, useReducer } from "react";
 
 function BookingPage() {
   const [availableTimes, setAvailableTimes] = useState([
@@ -11,6 +11,12 @@ function BookingPage() {
     "21:00",
     "22:00",
   ]);
+
+  const updateTimes = (date) => {
+    return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+  };
+
+  
 
   return (
     <div className="booking-page-container">
