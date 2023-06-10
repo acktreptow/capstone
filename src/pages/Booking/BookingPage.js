7f733284a59c11f23c12dbb9e38649e903110f44
acktreptow <acktreptow@gmail.com>
2023-06-10 06:15:56 +0100

    add reducer

--- src/pages/Booking/BookingPage.js
+++ src/pages/Booking/BookingPage.js
@@ -3,15 +3,6 @@ import BookingForm from "./BookingForm";
 import { useState, useReducer } from "react";
 
 function BookingPage() {
-  const [availableTimes, setAvailableTimes] = useState([
-    "17:00",
-    "18:00",
-    "19:00",
-    "20:00",
-    "21:00",
-    "22:00",
-  ]);
-
   const updateTimes = (date) => {
     return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
   };
@@ -20,13 +11,12 @@ function BookingPage() {
     return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
   };
 
+  const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes);
+
   return (
     <div className="booking-page-container">
       <h1 className="booking-page-title">Reserve a table</h1>
-      <BookingForm
-        availableTimes={availableTimes}
-        setAvailableTimes={setAvailableTimes}
-      />
+      <BookingForm availableTimes={availableTimes} dispatch={dispatch} />
     </div>
   );
 }
