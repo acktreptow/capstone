9759b9e7fad3f88f9c22fa67f2d63df44a57ba21
acktreptow <acktreptow@gmail.com>
2023-06-10 05:50:39 +0100

    add initializeTimes function

--- src/pages/Booking/BookingPage.js
+++ src/pages/Booking/BookingPage.js
@@ -16,7 +16,9 @@ function BookingPage() {
     return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
   };
 
-  
+  const initializeTimes = () => {
+    return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+  };
 
   return (
     <div className="booking-page-container">
